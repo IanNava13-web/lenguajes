5d1f09c54fa64500c648188e7863c1d8e385a369
BrahianENC <benc13github@gmail.com>
2024-09-29 22:14:49 -0500

    promedio

---
+++ Python/f_promedio.py
@@ -0,0 +1,8 @@
+def promedio(a,b,c):
+    return (a+b+c)/3
+
+a = float(input("Ingrese primer numero: "))
+b = float(input("Ingrese segundo numero: "))
+c = float(input("Ingrese tercer numero: "))
+
+print("El promedio es: ",promedio(a,b,c))
