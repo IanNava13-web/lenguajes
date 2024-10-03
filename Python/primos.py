27bca614106d6cdb89733fb660968fcaad8b1c12
BrahianENC <benc13github@gmail.com>
2024-10-02 22:24:30 -0500

    primos

---
+++ Python/primos.py
@@ -0,0 +1,14 @@
+def es_primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+# Ejemplo de uso
+numero = int(input("Introduce un número: "))
+if es_primo(numero):
+    print(f"{numero} es un número primo.")
+else:
+    print(f"{numero} no es un número primo.")
