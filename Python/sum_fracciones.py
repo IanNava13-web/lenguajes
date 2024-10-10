aaec32287816c89427c62657f4a83025af81fddd
BrahianENC <benc13github@gmail.com>
2024-10-09 22:59:12 -0500

    sum fracciones

---
+++ Python/sum_fracciones.py
@@ -0,0 +1,11 @@
+from fractions import Fraction
+
+# Define las fracciones
+fraccion1 = Fraction(1, 2)  # 1/2
+fraccion2 = Fraction(1, 3)  # 1/3
+
+# Suma las fracciones
+resultado = fraccion1 + fraccion2
+
+# Muestra el resultado
+print(f"La suma de {fraccion1} y {fraccion2} es {resultado}")
