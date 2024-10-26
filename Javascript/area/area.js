d4f993207e4ddc6b2fc75154517bd2d42c469f1d
IanNava13-web <benc13github@gmail.com>
2024-10-25 20:24:49 -0500

    prueba js

---
+++ Javascript/area/area.js
@@ -0,0 +1,9 @@
+let base = 4
+let altura = 10 
+
+let area = (base*altura)/2
+
+console.log(typeof base)
+console.log(typeof altura)
+console.log(typeof area)
+console.log("El area del triangulo es: ", area)
