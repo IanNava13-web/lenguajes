f1280ff8896a041cb636d9be4c574775e5571ca4
IanNava13-web <benc13github@gmail.com>
2024-11-21 19:19:19 -0500

    example

--- Typescript/build/decoradores.js
+++ Typescript/build/decoradores.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 console.log("Decoradores");
 /*
 
@@ -31,14 +34,14 @@ Tipos de decoradores
 function Logger(constructor) {
     console.log(`Clase registrada: ${constructor.name}`);
 }
-let MiClase = class MiClase {
+let MiClase1 = class MiClase1 {
     constructor() {
         console.log('Instancia creada');
     }
 };
-MiClase = __decorate([
+MiClase1 = __decorate([
     Logger
-], MiClase);
+], MiClase1);
 // Decoradores de Método
 function Log(target, propertyKey, descriptor) {
     const originalMethod = descriptor.value;
@@ -47,11 +50,37 @@ function Log(target, propertyKey, descriptor) {
         return originalMethod.apply(this, args);
     };
 }
-class MiClase {
+class MiClase2 {
     metodo(param) {
         console.log(`Ejecutando metodo con ${param}`);
     }
 }
 __decorate([
     Log
+], MiClase2.prototype, "metodo", null);
+// Decoradores de Propiedades
+function ReadOnly(target, propertyKey) {
+    Object.defineProperty(target, propertyKey, {
+        writable: false
+    });
+}
+class MiClase3 {
+    constructor() {
+        this.propiedad = "Soy una propiedad inmutable";
+    }
+}
+__decorate([
+    ReadOnly
+], MiClase3.prototype, "propiedad", void 0);
+// Decoradores de Parámetros
+function LogParam(target, propertyKey, parameterIndex) {
+    console.log(`Parámetro decorado en ${propertyKey}, índice: ${parameterIndex}`);
+}
+class MiClase {
+    metodo(param1, param2) {
+        console.log(param1, param2);
+    }
+}
+__decorate([
+    __param(0, LogParam)
 ], MiClase.prototype, "metodo", null);
