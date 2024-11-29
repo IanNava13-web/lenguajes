3c71e99619b4c8ca055e62e6aac00775f30f9df7
IanNava13-web <benc13github@gmail.com>
2024-11-28 23:05:08 -0500

    sql

---
+++ SQL/recurso.sql
@@ -0,0 +1,19 @@
+-- Tipos de dato
+-- Para uso de recursos como la memoria
+
+-- Numericos:
+-- INT
+-- BIGINT
+-- FLOAT
+
+-- Alphanumerico:
+-- CHAR
+-- VARCHAR
+-- TEXT
+
+-- Fecha y hora:
+-- DATE
+-- TIME
+-- DATATIME
+-- TIMESTAMP
+-- YEAR
